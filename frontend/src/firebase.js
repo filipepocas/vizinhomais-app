90f7b598b748ff697456aa40d1c148d4b201529a
filipepocas <rochap.filipe@gmail.com>
2026-02-18 19:12:44 +0000

    FIX: Final deploy with root directory and env vars

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-apiKey: "AIzaSyBwZSP9g7kJb_djqx_1-tHdtgO3XLChm6E",
-authDomain: "vizinhomais.firebaseapp.com",
-projectId: "vizinhomais",
-storageBucket: "vizinhomais.firebasestorage.app",
-messagingSenderId: "626292460867",
-appId: "1:626292460867:web:4d18ab05ebb88c4bbcf3f6",
-measurementId: "G-GZ2H4YW2Q4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
